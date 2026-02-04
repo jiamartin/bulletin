60f1310540056fc0252299c3f24380fd2e3496e3
Julia Lagarde <julialagarde3@gmail.com>
2026-02-05 00:21:09 +0800

    Setup MongoDB

---
+++ backend/models/Note.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+// Schema Creation
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+    },
+    { timestamps : true } //createdAt, updatedAt
+);
+
+//Model Creation
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note
