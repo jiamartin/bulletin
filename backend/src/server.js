fd3dbba0165a4ccc7edcde8b1b3c6da5101e4673
Julia Lagarde <julialagarde3@gmail.com>
2026-02-05 22:52:07 +0800

    Setup Updstash and React

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "../config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "../middleware/ratelimiter.js";
 
 dotenv.config();
 
@@ -11,10 +12,19 @@ const PORT = process.env.PORT || 5001
 connectDB();
 
 // middleware
-app.use(express.json())
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+//custom middleware
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// })
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server started on PORT: ", PORT);
-}); 
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT: ", PORT);
+    });
+});
