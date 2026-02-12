b199f4abba5fb1c70cc8152be47167b3227990b1
Julia Lagarde <julialagarde3@gmail.com>
2026-02-13 00:20:10 +0800

    Update Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <div className="text-center text-primary py-10">Loading notes...</div> }
+        {loading && <div className="text-center text-primary py-10">Loading all notes...</div> }
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
