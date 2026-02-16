044348019ce430b73435c64941d6bb5a8ebc5100
Julia Lagarde <julia.lagarde@caspio.com>
2026-02-16 23:15:58 +0800

    Note details page and Note deletion

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast"
 
 import Navbar from "../components/Navbar"
 import RateLimitedUI from "../components/RateLimitedUI";
 import NoteCard from "../components/NoteCard";
 import api from "../lib/axios";
+import toast from "react-hot-toast"
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -41,12 +42,12 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading all notes...</div> }
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <div>
-                <NoteCard key={note.id} note={note} />
-              </div>
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
 
           </div>
